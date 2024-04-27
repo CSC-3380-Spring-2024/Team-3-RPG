57d4d8675feba715fee274088a3d356994091c70
grishma-a <108956544+grishma-a@users.noreply.github.com>
2024-04-27 18:53:11 -0500

    altered giveQuest text
    changed the lines in the give quest, we will focus on more game-like text

--- Assets/IntroductionPrompt.cs
+++ Assets/IntroductionPrompt.cs
@@ -6,6 +6,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "IntroductionPrompt", menuName = "ScriptableObjects/IntroductionPrompt", order = 1)]
 public class IntroductionPrompt : ScriptableObject
 {
-    public static string[] lines = { "WELCOME TO ELYSSIU!", "You are a new student of the academy Elyssiu training to become a combat sorcerer/sorceress (CS Major)", "After your orientation, you meet with one of the head wizards, Lord Jonathan Lucas Dennington, who wants to be your personal mentor", "Soon after, you learn you are the fabled hero of a prophecy tasked to slay Mikhail, Beast of the Ancient, located in the Ruins of Tiger Colosseum", "Set forth, unlock the secrets of Elyssiu and fulfill YOUR prophecy!" };    // Array of dialogue lines for the IntroductionPrompt
+    public static string[] lines = { "WELCOME TO ELYSSIU!", "You are a new student of the academy Elyssiu training to become a combat sorcerer/sorceress (CS Major)", "After your orientation, you meet with one of the head wizards, Lord Jonathan Lucas Dennington, who wants to be your personal mentor", "Soon after, you learn you are the fabled hero of a prophecy tasked to slay Sister Cindy", "Set forth, unlock the secrets of Elyssiu and fulfill YOUR prophecy!" };    // Array of dialogue lines for the IntroductionPrompt
     public float textSpeed;  // Speed at which the text is displayed
 }
