1afb04c56c33c5c8dd5d5f87ca5ddf736be43f85
grishma-a <108956544+grishma-a@users.noreply.github.com>
2024-04-30 09:01:20 -0500

    adding comments
    added comments to help readaibility + i accidentally pushed testing into dialogue instead of the other way around so i added comments to get the changes back

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -6,100 +6,100 @@ using TMPro;
 public class Dialogue : MonoBehaviour
 {
     public GameObject dialogueBox; // Reference to the GameObject containing the dialogue box UI elements
-    public TextMeshProUGUI textComponent;
-    public IntroductionPrompt IntroductionPrompt;
-    public GiveQuestPrompt GiveQuestPrompt;
-    public SisterCindy SisterCindy;
+    public TextMeshProUGUI textComponent; // this is how unity sees the text
+    public IntroductionPrompt IntroductionPrompt; // getting the introduction prompt
+    public GiveQuestPrompt GiveQuestPrompt;// getting the first quest prompt - jld
+    public SisterCindy SisterCindy; // getting the sister cindy prompt
     public float textSpeed = 0.05f; // Default text speed value
-    private string[] introductionLines;
-    private string[] giveQuestLines;
-    private string[] sisterCindylines;
-    private bool introCompleted = false; // Indicates if the intro dialogue is completed
-    private bool giveQuestCompleted = false;
-    private bool sisterCindyCompleted = false;
-    private bool waitingForSpace = false; // Indicates if the script is waiting for space bar input
-    private bool instantFinish = false;
+    private string[] introductionLines; // getting the lines from intro
+    private string[] giveQuestLines;// getting the lines from jld
+    private string[] sisterCindylines;// getting the lines from sister cindy
+    private bool introCompleted = false; //  if the intro dialogue is completed
+    private bool giveQuestCompleted = false; // these are used to make sure the order is correct -- for questing
+    private bool sisterCindyCompleted = false; // need to add mrore dialogue promots, not used now will be used later
+    private bool waitingForSpace = false; //  if the script is waiting for space bar input
+    private bool instantFinish = false; // if user puts spacebar before line is finished
 
     void Start()
     {
-        introductionLines = IntroductionPrompt.lines;
-        giveQuestLines = GiveQuestPrompt.lines;
-        sisterCindylines = SisterCindy.lines;
+        introductionLines = IntroductionPrompt.lines; // intro
+        giveQuestLines = GiveQuestPrompt.lines;//give quest
+        sisterCindylines = SisterCindy.lines;//sister cindy
 
-        StartCoroutine(ShowDialogue(introductionLines));
+        StartCoroutine(ShowDialogue(introductionLines));//intro starts immediately - no more button click for it
     }
 
     void Update()
     {
-        if (waitingForSpace && Input.GetKeyDown(KeyCode.Space))
+        if (waitingForSpace && Input.GetKeyDown(KeyCode.Space)) // this is for going to the next line
         {
-            // If waiting for space and space bar is pressed, proceed to the next line
             waitingForSpace = false;
-            //NextLine();
+            //  waiting for space and space bar is pressed, proceed to the next line
         }
-        if (instantFinish == false && Input.GetKeyDown(KeyCode.Space))
+        if (instantFinish == false && Input.GetKeyDown(KeyCode.Space)) // this is if the space bar is clicked during the character by character display
         {
-            instantFinish = true;
+            instantFinish = true; // finish the line of text
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && introCompleted && giveQuestCompleted)
+        if (Input.GetKeyDown(KeyCode.E) && introCompleted && giveQuestCompleted) // e is for the next prompts
         {
-            // If intro is completed and "E" is pressed, proceed to the give quest prompt
+            //  intro is completed and "E" is pressed, proceed to the sister cindy  prompt
             StartCoroutine(ShowDialogue(sisterCindylines));
         }
 
-        // Check for "E" key to proceed to the give quest prompt
+        //  "E" key to proceed to the give quest prompt
         else if (Input.GetKeyDown(KeyCode.E) && introCompleted)
         {
-            // If intro is completed and "E" is pressed, proceed to the give quest prompt
+            //  intro is completed and "E" is pressed, proceed to the give quest prompt
             StartCoroutine(ShowDialogue(giveQuestLines));
         }
 
     }
     IEnumerator ShowDialogue(string[] lines)
     {
-        dialogueBox.SetActive(true); // Activate the dialogue box
+        dialogueBox.SetActive(true); // Activate the dialogue box to display text
 
-        for (int i = 0; i < lines.Length; i++)
+        for (int i = 0; i < lines.Length; i++)//for all the lines in the prompt
         {
-            textComponent.text = string.Empty;
-            instantFinish = false;
+            textComponent.text = string.Empty; // make it clean
+            instantFinish = false; // dont finish yet
 
-            // Display the current line character by character
-            foreach (char c in lines[i].ToCharArray())
+            // display the current line character by character
+            foreach (char c in lines[i].ToCharArray())// all characters
             {
-                textComponent.text += c;
-                yield return new WaitForSeconds(textSpeed); // Use textSpeed variable
-                if (instantFinish == true)
+                textComponent.text += c;//add/display character 
+                yield return new WaitForSeconds(textSpeed); // Use textSpeed variable to let it display at a set rate
+                if (instantFinish == true) // space is clicked
                 {
-                    break;
+                    break;//break out of the char loop
                 }
             }
-            textComponent.text = lines[i];
+            textComponent.text = lines[i];//set the textbox to have the whole line
 
-            // Set waiting for space to true after displaying the line
+            // set waiting for space to true after displaying the line
             waitingForSpace = true;
 
-            // Wait until space bar is pressed to proceed to the next line
+            // wait until space bar is pressed to proceed to the next line
             while (waitingForSpace)
             {
-                yield return null;
+                yield return null;//do nothing if waiting for space
             }
+            //if waiting for space is false, exits loop, and restarts with the next line in the dialogue
         }
 
-        if (lines == introductionLines)
+        if (lines == introductionLines) // set the intro as done
         {
-            introCompleted = true;
+            introCompleted = true;//done
 
         }
-        else if (lines == giveQuestLines)
+        else if (lines == giveQuestLines)// set jld as done
         {
-            giveQuestCompleted = true;
+            giveQuestCompleted = true;//done
         }
-        else if (lines == sisterCindylines)
+        else if (lines == sisterCindylines)// set the sister cindy as done
         {
-            sisterCindyCompleted = true;
-            dialogueBox.SetActive(false);
+            sisterCindyCompleted = true;//done
+            dialogueBox.SetActive(false);//hide the dialogue box
         }
     }
 }
--- Assets/SisterCindy.cs
+++ Assets/SisterCindy.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SisterCindy", menuName = "ScriptableObjects/SisterCindy", order = 1)]
+[CreateAssetMenu(fileName = "SisterCindy", menuName = "ScriptableObjects/SisterCindy", order = 1)]//create a scriptable object
 public class SisterCindy : ScriptableObject
 {
-    public static string[] lines = { "Challenge Sister Cindy!", "Sister Cindy has been a nuisance to our grounds for far too long!", "Get rid of her and get me a margarita.", "But you have to train before defeating her." };   // Array of dialogue lines for this prompt
+    public static string[] lines = { "Challenge Sister Cindy!", "Sister Cindy has been a nuisance to our grounds for far too long!", "Get rid of her and get me a margarita.", "But you have to train before defeating her." };// Array of dialogue lines for this prompt
     public float textSpeed;  // Speed at which the text is displayed
+    //sister cindy quest
 }
