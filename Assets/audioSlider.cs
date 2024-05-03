fe6c6ad2e66545d00cc75d545fade9930a241bfb
grishma-a <108956544+grishma-a@users.noreply.github.com>
2024-05-02 19:06:33 -0500

    audio has 2 sources
    separated audio from background and side effects

--- Assets/audioSlider.cs
+++ Assets/audioSlider.cs
@@ -7,38 +7,64 @@ using UnityEngine.UI;
 
 public class audioSlider : MonoBehaviour
 {
-    [SerializeField] Slider volumeSlider;
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "";
+    private static readonly string SoundEffectsPref = "";
 
-    private float musicVolume = 1f;
+    private int firstPlayInt;
+    public Slider backgroundSlider, soundEffectSlider;
+    private float backgroundFloat, soundEffectsFloat;
+
+    public AudioSource backgroundAudio;
+    public AudioSource soundEffectAudio;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        if (!PlayerPrefs.HasKey("musicVolume"))
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+        if (firstPlayInt == 0)
         {
-            PlayerPrefs.SetFloat("musicVolume", 1);
-            Load();
+            backgroundFloat = .125f;
+            soundEffectsFloat = .75f;
+            backgroundSlider.value = backgroundFloat;
+            soundEffectSlider.value = soundEffectsFloat;
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);
+            PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+
+
         }
         else
         {
-            Load();
+            backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backgroundFloat;
+            soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
+            soundEffectSlider.value = soundEffectsFloat;
         }
     }
 
-    public void ChangeVolume()
+    public void save()
     {
-        AudioListener.volume = volumeSlider.value;
-        Save();
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+        PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectSlider.value);
     }
 
-    private void Load()
+    void OnApplicationFocus(bool inFocus)
     {
-        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        if (!inFocus)
+        {
+            save();
+        }
     }
 
-    private void Save()
+    public void UpdateSound()
     {
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        backgroundAudio.volume = backgroundSlider.value;
+        soundEffectAudio.volume = soundEffectSlider.value;
+
     }
+
 
 
 }
