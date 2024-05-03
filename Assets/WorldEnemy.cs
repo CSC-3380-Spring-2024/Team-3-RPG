916247d76fa192acc10fcb25e98fd84ab1591407
Ricky Liang <liangricky7@gmail.com>
2024-05-03 09:16:44 -0500

    Create WorldEnemy
    initialize branch + worldenemy script

---
+++ Assets/WorldEnemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldEnemy : MonoBehaviour
+{
+    public GameObject[] enemies; //will store enemeis to bring into combatscene
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
