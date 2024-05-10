56e9bb5c7680bb7887fe8d3f24b14123aa305063
grishma-a <108956544+grishma-a@users.noreply.github.com>
2024-05-09 20:26:02 -0500

    small changes
    small changes

--- Assets/_Scripts/Footsteps.cs
+++ Assets/_Scripts/Footsteps.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Footsteps : MonoBehaviour
 {
     public AudioSource footstepsSound;
+    [SerializeField]
+    public PlayerController playerController;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
-            footstepsSound.enabled = true;
+            if (playerController != null && playerController.enabled)
+            {
+                footstepsSound.enabled = true;
+            }
+            //if hit wasd and the player can move then make the footsteps sound
+
         }
         else
         {
             footstepsSound.enabled = false;
+            //if not, do not play the footsteps sound
         }
+
     }
 }
